c64c6b99eed9fa05931378c7560d8c08b36bdc89
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-20 16:54:38 -0600

    CRUD for students complete, still need to change cohort_id display to corhort name

--- index.js
+++ index.js
@@ -104,5 +104,75 @@ server.delete('/api/cohorts/:id', async (req, res) => {
   } catch (error) {}
 });
 
+//list all students
+server.get('/api/students', async (req, res) => {
+    // get the students from the database
+    try {
+      const students = await db('students'); // all the records from the table
+      res.status(200).json(students);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+  // list a student by id
+server.get('/api/students/:id', async (req, res) => {
+    // get the students from the database
+    try {
+      const student = await db('students')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(student);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+// create student
+server.post('/api/students', async (req, res) => {
+    try {
+      const [id] = await db('students').insert(req.body);
+  
+      const student = await db('students')
+        .where({ id })
+        .first();
+  
+      res.status(201).json(student);
+    } catch (error) {
+      res.status(500).json({ message: 'student not found' });
+    }
+  });
+  // update student
+server.put('/api/students/:id', async (req, res) => {
+    try {
+      const count = await db('students')
+        .where({ id: req.params.id })
+        .update(req.body);
+  
+      if (count > 0) {
+        const student = await db('students')
+          .where({ id: req.params.id })
+          .first();
+  
+        res.status(200).json(student);
+      } else {
+        res.status(404).json({ message: 'Records not found' });
+      }
+    } catch (error) {}
+  });
+// remove student (inactivate the student)
+server.delete('/api/students/:id', async (req, res) => {
+    try {
+      const count = await db('students')
+        .where({ id: req.params.id })
+        .del();
+  
+      if (count > 0) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: 'Records not found' });
+      }
+    } catch (error) {}
+  });  
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
